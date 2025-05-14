9310c980d0a72b0b39ed95aac3e3c3b639848fc4
23navi <navisureka23@gmail.com>
2025-05-14 11:43:04 +0530

    postgres operator

--- Setup/dags/user_processing.py
+++ Setup/dags/user_processing.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from datetime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 with DAG(
     dag_id="user_processing",
@@ -7,4 +8,10 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     catchup=False,
 ) as dag:
-    pass
+    create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id="postgres_dag1",
+        sql="""
+            CREATE TABLE IF NOT EXISTS users (firstname TEXT NOT NULL,lastname TEXT NOT NULL,country TEXT NOT NULL,username TEXT NOT NULL,password TEXT NOT NULL,email TEXT NOT NULL);
+        """,
+    )
